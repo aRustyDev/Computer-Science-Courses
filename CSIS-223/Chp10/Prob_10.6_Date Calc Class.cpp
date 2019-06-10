9e491966fac3d1fe24c08a7f371ac30b85642403
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-06-10 09:11:42 -0700

    Create Prob_10.6_Date Calc Class.cpp

---
+++ CSIS-223/Chp10/Prob_10.6_Date Calc Class.cpp
@@ -0,0 +1,30 @@
+/*      Title: Date Calc Class | Problem 10.6
+ *      Author: Adam C Smith | RedGrinGrumbler
+ *
+ *              ::Compiler Info::
+ *      CMD: cpp --version
+ *          Apple LLVM version 10.0.1 (clang-1001.0.46.4)
+ *          Target: x86_64-apple-darwin18.6.0
+ *          Thread model: posix
+ *      CMD: gcc --version
+ *          Apple LLVM version 10.0.1 (clang-1001.0.46.4)
+ *          Target: x86_64-apple-darwin18.6.0
+ *          Thread model: posix
+ *
+ *      GitHub: https://github.com/RedGrinGrumbler/Computer-Science-Courses/blob/master/CSIS-223/Chp9/Prob_9.4_Order%20Menu.cpp
+ *
+ *      Purpose: .
+ *
+ * */
+/*      Notes/To-Do's:
+ *
+ *
+ *
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
