0316d79e344a9d80fbdf66c8b6245699f6d99d38
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-06-10 10:32:08 -0700

    Added Initial Functions & Constructors

--- CSIS-223/Chp10/Prob_10.6_Date Calc Class.cpp
+++ CSIS-223/Chp10/Prob_10.6_Date Calc Class.cpp
@@ -1,5 +1,5 @@
 /*      Title: Date Calc Class | Problem 10.6
- *      Author: Adam C Smith | RedGrinGrumbler
+ *      Author: Adam Smith | RedGrinGrumbler
  *
  *              ::Compiler Info::
  *      CMD: cpp --version
@@ -17,14 +17,41 @@
  *
  * */
 /*      Notes/To-Do's:
- *
- *
+ *          'Set Day' Function
+ *          'Print Day' Function
+ *          'Return Day' Function
+ *          'Return Next Day' Function
+ *          'Return Previous Day' Function
+ *          'Add Days' Function
+ *          add appropriate constructors
  *
 */
 
 #include <iostream>
 
+using namespace std;
+
+class dayType{
+    public:
+        //Constructors
+        dayType(string dayName, int julianDate){}//Parameterized - DayName, JulianDate
+        dayType(string dayName){}//Parameterized - DayName
+        dayType(){}//default
+    
+        //Accessor Functions
+        void getDay(){}
+        void printDay(){}
+        void getTomorrow(){}
+        void getYesterday(){}
+    
+        //Mutator Functions
+        void setDay(){}
+        void addDays(int daysAdded){}
+    
+    private:
+        string dayName(){}
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
