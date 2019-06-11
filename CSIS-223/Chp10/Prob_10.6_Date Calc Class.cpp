30f13d392ef3d0d3353843cedb6d95322c1ede88
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-06-11 00:32:24 -0700

    Working Version
    Still would like to get it set up to work when Jan 1 != Monday;

--- CSIS-223/Chp10/Prob_10.6_Date Calc Class.cpp
+++ CSIS-223/Chp10/Prob_10.6_Date Calc Class.cpp
@@ -13,7 +13,7 @@
  *
  *      GitHub: https://github.com/RedGrinGrumbler/Computer-Science-Courses/blob/master/CSIS-223/Chp10/Prob_10.6_Date%20Calc%20Class.cpp
  *
- *      Purpose: .
+ *      Purpose: Handle dayType Object by using Classes
  *
  * */      //Specifications Block
 /*      Notes/To-Do's:
@@ -25,12 +25,17 @@
  *              'Return Previous Day'
  *              'Add Days'
  *          * add appropriate constructors
+ *          The use of Julian Dates makes for a simple calculator, however it assumes the year start on Monday.
+ *          This may not always be true, this can easily be solved by implementing a initial offset for what ever
+ *          day the year started on. I have not implemented this because I am not planning on taking this to any
+ *          production level.
  *
  *          [Q]: Do I need multiple types of constructors besides Default and Full?
  *
  * */      //Notes / To-Do's Block
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -78,46 +83,57 @@ class dayType{
                 *///Parameterized - DayName, JulianDate
 
             //Accessor Functions - Definitions
-            string getDay();
+            void printDay();
+                /*
+     *
+    */
+            dayType* getDay();
                 /*  Function to return the current dayType Objects dayName value.
                  *
                  *  Post-Condition:
                  *
                 */
-            void printDay();
+            dayType getTomorrow();
                 /*
                  *
                 */
-            void getTomorrow();
-                /*
-                 *
-                */
-            void getYesterday();
+            dayType getYesterday();
                 /*
                  *
                 */
 
             //Mutator Functions - Definitions
-            void setDay();
-                /*
+            void setDay(string);
+                /*  Function to change the dayType Object
                  *
-                */
+                 *  Post-Condition:
+                 *      dayType Object
+                 *      * day value of STRING
+                 *      * julian value of INT
+                 *
+                *///DONE
+            void setJulian(int);
             void addDays(int daysAdded);
-                /*
+                /*  Function adds INT days to current julianDate Value
+                 *
+                 *  Post-Condition:
+                 *
                  *
                 */
+            //Processor Functions
+            string julianToString();
+            int dayToJulian();
 
-        private:
-            //Class Variables - Definitions
+            //variables
             int julianDate;
-                /*
-                 *
-                */
+            /*  Int Variable that holds the Julian Date
+             *
+            */
             string dayName;
-                /*
-                 *
-                */
 };
+
+//Global Variables
+    int dayOffset = 0;//Used to offset the dates if Jan 1 != Monday, Default is Monday (0)
 
 //Constructors - Implementations
     dayType::dayType(){
@@ -134,20 +150,135 @@ class dayType{
 }//Parameterized - DayName, JulianDate
 
 //Functions - Implementations
-    //Accessor Functions
-    string dayType::getDay(){
-        return this->dayName;
+    //Processor Functions
+    string dayType::julianToString(){
+        int temp = this->julianDate + dayOffset;
+        switch (temp % 7){
+            case 0:
+                return "Sunday";
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+        }
     }
-    void dayType::printDay(){}
-    void dayType::getTomorrow(){}
-    void dayType::getYesterday(){}
+    int dayType::dayToJulian(){
+        int julian = dayOffset ;//Would relate to offset.
+        if(this->dayName == "Sunday" || this->dayName == "SUN"){            return 7;
+        }else if(this->dayName == "Monday" || this->dayName == "MON"){      return 1;
+        }else if(this->dayName == "Tuesday" || this->dayName == "TUE"){     return 2;
+        }else if(this->dayName == "Wednesday" || this->dayName == "WED"){   return 3;
+        }else if(this->dayName == "Thursday" || this->dayName == "THU"){    return 4;
+        }else if(this->dayName == "Friday" || this->dayName == "FRI"){      return 5;
+        }else if(this->dayName == "Saturday" || this->dayName == "SAT"){    return 6;
+    }
+}//Not sure how to include dayOffset Here.
+
+    //Accessor Functions
+    void dayType::printDay(){
+        printf("Val of dayType: ");
+        cout << this->dayName << endl;
+    }
+    dayType* dayType::getDay(){
+        return this;
+    }
+    dayType dayType::getTomorrow(){
+        dayType day = dayType();
+        day.setDay(this->dayName);
+        day.setJulian(day.dayToJulian());
+
+        day.julianDate = day.julianDate + 1;
+        day.dayName = day.julianToString();
+        printf("\nToday:\n\t");
+        this->printDay();
+        printf("Tomorrow:\n\t");
+        day.printDay();
+        return day;
+    }
+    dayType dayType::getYesterday(){
+        dayType day = dayType();
+        day.setDay(this->dayName);
+        day.setJulian(day.dayToJulian());
+
+        day.julianDate = day.julianDate - 1;
+        day.dayName = day.julianToString();
+        printf("\nToday:\n\t");
+        this->printDay();
+        printf("Yesterday:\n\t");
+        day.printDay();
+        return day;
+    }
 
     //Mutator Functions
-    void dayType::setDay(){}
-    void dayType::addDays(int daysAdded){}
+    void dayType::setDay(string day = "Monday"){
+        this->dayName = day;
+    }
+    void dayType::setJulian(int julian = 1){
+    this->julianDate = julian;
+}
+    void dayType::addDays(int daysAdded){
+        this->julianDate = this->julianDate + daysAdded;
+        this->dayName = this->julianToString();
+        printf("\n%d Day's From Today:\n\t", daysAdded);
+        this->printDay();
+    }
+
+    //Input Functions
+    dayType makeDay(){
+
+        //Variables
+        string dayAbrv;
+
+        //Input
+        printf("MON | TUE | WED | THU | FRI | SAT | SUN\n");
+        printf("Enter 3 letter abbreviation of day: ");
+
+        if(cin.peek() == '\n'){
+            getline(cin.ignore(), dayAbrv);
+        }else {
+            getline(cin, dayAbrv);
+        }
+
+        //Processing
+        for(int i = 0; i < dayAbrv.size(); i++){
+            if (!isalpha(dayAbrv[i])){
+                cout << "Input Error: isAlpha Catch" << endl;
+                break;
+            }
+            dayAbrv[i] = toupper(dayAbrv[i]);
+        }
+
+        dayType day = dayType();            //Make new Obj
+        day.setDay(dayAbrv);                //Set Obj.string = input
+        day.setJulian(day.dayToJulian());   //Use Obj.string to set Obj.Int = 'correct Int'
+        day.setDay(day.julianToString());   //Use Corrected Obj.Int to get full Obj.string.
+
+        //Output
+        return day;
+
+    }
 
 //Main Fn
 int main(){
 
+        int daysToMove = 0;
+        dayType day = makeDay();
+        cout << "Today is " << day.dayName << endl;
+
+        cout << "--Move 'x' Days--\n\tx=:";
+        cin >> daysToMove;
+        day.addDays(daysToMove);
+
+        day.getYesterday();
+
+        day.getTomorrow();
     return 0;
 }
