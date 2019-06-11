59a59629257203f30b850a4908c67b4538c99ec3
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-06-10 20:29:48 -0700

    Update Prob_10.6_Date Calc Class.cpp

--- CSIS-223/Chp10/Prob_10.6_Date Calc Class.cpp
+++ CSIS-223/Chp10/Prob_10.6_Date Calc Class.cpp
@@ -15,43 +15,139 @@
  *
  *      Purpose: .
  *
- * */
+ * */      //Specifications Block
 /*      Notes/To-Do's:
- *          'Set Day' Function
- *          'Print Day' Function
- *          'Return Day' Function
- *          'Return Next Day' Function
- *          'Return Previous Day' Function
- *          'Add Days' Function
- *          add appropriate constructors
+ *          * Functions for ...
+ *              'Set Day'
+ *              'Print Day'
+ *              'Return Day'
+ *              'Return Next Day'
+ *              'Return Previous Day'
+ *              'Add Days'
+ *          * add appropriate constructors
  *
-*/
+ *          [Q]: Do I need multiple types of constructors besides Default and Full?
+ *
+ * */      //Notes / To-Do's Block
 
 #include <iostream>
 
 using namespace std;
 
+//Classes
 class dayType{
-    public:
-        //Constructors
-        dayType(string dayName, int julianDate){}//Parameterized - DayName, JulianDate
-        dayType(string dayName){}//Parameterized - DayName
-        dayType(){}//default
-    
-        //Accessor Functions
-        void getDay(){}
-        void printDay(){}
-        void getTomorrow(){}
-        void getYesterday(){}
-    
-        //Mutator Functions
-        void setDay(){}
-        void addDays(int daysAdded){}
-    
-    private:
-        string dayName(){}
+    //Declarations
+        public:
+            //Constructors - Definitions
+            dayType();
+                /*  Default Constructor.
+                 *
+                 *  Sets the Default Value of dayName & julianDate
+                 *
+                 *  Default Values
+                 *      dayName = "Monday"
+                 *      julianDate = 1
+                 *
+                 *  Post-Condition:
+                 *      dayName = "Monday"
+                 *      julianDate = 1
+                 *
+                *///default
+            dayType(string);
+                /*  Parameterized Constructor. | DayName, JulianDate
+                 *
+                 *  Sets the Default Value of dayName & julianDate
+                 *
+                 *  Default Values
+                 *      julianDate = 1 (Default)
+                 *
+                 *  Post-Condition:
+                 *      dayName = string
+                 *      julianDate = 1  (Default)
+                 *
+                *///Parameterized - DayName
+            dayType(string, int);
+                /*  Parameterized Constructor. | DayName, JulianDate
+                 *
+                 *  Sets the Default Value of dayName & julianDate
+                 *
+                 *  Post-Condition:
+                 *      dayName = string
+                 *      julianDate = int
+                 *
+                *///Parameterized - DayName, JulianDate
+
+            //Accessor Functions - Definitions
+            string getDay();
+                /*  Function to return the current dayType Objects dayName value.
+                 *
+                 *  Post-Condition:
+                 *
+                */
+            void printDay();
+                /*
+                 *
+                */
+            void getTomorrow();
+                /*
+                 *
+                */
+            void getYesterday();
+                /*
+                 *
+                */
+
+            //Mutator Functions - Definitions
+            void setDay();
+                /*
+                 *
+                */
+            void addDays(int daysAdded);
+                /*
+                 *
+                */
+
+        private:
+            //Class Variables - Definitions
+            int julianDate;
+                /*
+                 *
+                */
+            string dayName;
+                /*
+                 *
+                */
 };
 
-int main() {
+//Constructors - Implementations
+    dayType::dayType(){
+    dayName = "Monday";
+    julianDate = 1;
+}//default
+    dayType::dayType(string dayName){
+        this->dayName = dayName;
+        julianDate = 1;
+    }//Parameterized - DayName
+    dayType::dayType(string dayName, int julianDate){
+    this->dayName = dayName;
+    this->julianDate = julianDate;
+}//Parameterized - DayName, JulianDate
+
+//Functions - Implementations
+    //Accessor Functions
+    string dayType::getDay(){
+        return this->dayName;
+    }
+    void dayType::printDay(){}
+    void dayType::getTomorrow(){}
+    void dayType::getYesterday(){}
+
+    //Mutator Functions
+    void dayType::setDay(){}
+    void dayType::addDays(int daysAdded){}
+
+//Main Fn
+int main(){
+
     return 0;
 }
