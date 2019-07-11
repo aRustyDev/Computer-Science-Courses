0ce3a7caaa3d0351423962533da9091bcf0ff265
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-07-10 23:00:15 -0500

    Update Prob_13.03_Main_Box Type.cpp

--- CSIS-223/Chp13/Prob_13.03_Main_Box Type.cpp
+++ CSIS-223/Chp13/Prob_13.03_Main_Box Type.cpp
@@ -1,3 +1,21 @@
+/*      Title: boxType.h | Problem 13.03
+ *      Author: Adam Smith | RedGrinGrumbler
+ *
+ *              ::Compiler Info::
+ *      CMD: cpp --version
+ *          Apple LLVM version 10.0.1 (clang-1001.0.46.4)
+ *          Target: x86_64-apple-darwin18.6.0
+ *          Thread model: posix
+ *      CMD: gcc --version
+ *          Apple LLVM version 10.0.1 (clang-1001.0.46.4)
+ *          Target: x86_64-apple-darwin18.6.0
+ *          Thread model: posix
+ *
+ *      GitHub: https://github.com/RedGrinGrumbler/Computer-Science-Courses/blob/master/CSIS-223/Chp13
+ *
+ *      Purpose: Define the Class for boxType, so as to practice with overloading functions.
+ *
+ */
 #include <iostream>
 #include "boxType.h"
 
