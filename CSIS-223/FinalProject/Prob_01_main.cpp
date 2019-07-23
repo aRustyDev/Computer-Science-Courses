1c1e0e06e2a8b2f1d0b57554cdc3afc2865006f9
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-07-23 00:10:44 -0500

    Update Prob_01_main.cpp

--- CSIS-223/FinalProject/Prob_01_main.cpp
+++ CSIS-223/FinalProject/Prob_01_main.cpp
@@ -1,3 +1,22 @@
+/*      Title: carClass.cpp | Problem 15.02
+ *      Author: Adam Smith | RedGrinGrumbler
+ *
+ *              ::Compiler Info::
+ *      CMD: cpp --version
+ *          Apple LLVM version 10.0.1 (clang-1001.0.46.4)
+ *          Target: x86_64-apple-darwin18.6.0
+ *          Thread model: posix
+ *      CMD: gcc --version
+ *          Apple LLVM version 10.0.1 (clang-1001.0.46.4)
+ *          Target: x86_64-apple-darwin18.6.0
+ *          Thread model: posix
+ *
+ *      GitHub: https://github.com/RedGrinGrumbler/Computer-Science-Courses/blob/master/CSIS-223/FinalProject
+ *
+ *      Purpose: Final Project, create a car class that 'simulates a car'.
+ *
+ */
+
 #include <iostream>
 #include "carClass.h"
 
